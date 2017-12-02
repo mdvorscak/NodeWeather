ad3e04102e23aeaf63a6a03585660fe3a12a404d
Mike Dvorscak <mike.dvorscak@gmail.com>
2017-12-02 14:50:18 -0600

    Adding weather formatting

--- weatherService.js
+++ weatherService.js
@@ -4,14 +4,14 @@ function getForecastURL(APIKey, lat, lon){
     return "https://api.forecast.io/forecast/" + APIKey + "/" + lat + "," + lon;
 }
 
-function display(weatherObj){
-    console.dir(weatherObj.currently);
+function formatCurrentWeather(weather){
+    return `It is currently ${weather.summary.toLowerCase()} with a temperature of ${Math.round(weather.temperature)}°F`;
 }
 
 async function getCurrentWeather(APIKey, lat, lon){
     var forecastURL = getForecastURL(APIKey, lat, lon);
     var weatherObj = await webService.call(forecastURL);
-    display(weatherObj);
+    weatherObj.formatted = formatCurrentWeather(weatherObj.currently);
     return weatherObj;
 }
 
